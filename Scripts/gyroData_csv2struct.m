04ad2550a476b2d54368eeb782155191d227f402
Carlos Martinez <carlos.martinez.donate@gmail.com>
2023-04-01 23:22:53 +0200

    modificaciones en la funcion para permitir su uso en version 2018a (revisar que los cambios no afecten a otras versiones de matlab y comprobar que el output es el mismo)

--- Scripts/gyroData_csv2struct.m
+++ Scripts/gyroData_csv2struct.m
@@ -1,9 +1,20 @@
 
 function gyroData = gyroData_csv2struct(inputcsv_path)
 % inputcsv path
-gyroData_raw = readmatrix(inputcsv_path);
-gyroData.time = gyroData_raw(:,3);
-gyroData.x = gyroData_raw(:,4);
-gyroData.y = gyroData_raw(:,5);
-gyroData.z = gyroData_raw(:,6);
+try
+    % he tenido que añadir el try por si no esta la funcion en otra version
+    % de matlab
+    gyroData_raw = readmatrix(inputcsv_path);
+    gyroData.time = gyroData_raw(:,3);
+    gyroData.x = gyroData_raw(:,4);
+    gyroData.y = gyroData_raw(:,5);
+    gyroData.z = gyroData_raw(:,6);
+catch
+    gyroData_raw = dlmread(inputcsv_path, ',', 1,2);
+    gyroData.time = gyroData_raw(:,1);
+    gyroData.x = gyroData_raw(:,2);
+    gyroData.y = gyroData_raw(:,3);
+    gyroData.z = gyroData_raw(:,4);
+end
+
 end
