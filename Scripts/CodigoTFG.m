3ad47e984ffc7d69273798ddb8b7e5057c67d6f1
LAPTOP-SIE6E93T\almud <almudenamardonate@gmail.com>
2023-05-13 12:43:33 +0200

    codigo que grafica todos los pacientes directamente

--- Scripts/CodigoTFG.m
+++ Scripts/CodigoTFG.m
@@ -22,159 +22,167 @@ dataFolder_all = fullfile(TFG_git_path, 'gyroData', 'gyroDataNew');
 sportType = 'Squats';
 angleType = 'Giroscopio';
 location = 'Delante';
-patient = 'ALBA';
-dataFolder = fullfile(dataFolder_all, patient, sportType, angleType, location);
 
-dataFiles = dir(fullfile(dataFolder, '*.csv'));    
+% Patient list creation
+PatientList = dir(dataFolder_all);
+PatientList = PatientList(~ismember({PatientList.name}, {'.','..'}));
+PatientList = {PatientList.name};
 
-% Crear una variable de celdas para almacenar los datos de los archivos CSV
-data_cell = cell(length(dataFiles), 1); 
-DictPath = fullfile(dataFolder,strcat('DataDict_', sportType, '_', angleType, '.mat'));
+% patient = 'ALBA';
+for iPatient = 1:numel(PatientList)
 
-%% CARGAR .MAT
-% Mirar si existe diccionario
-if exist(DictPath)
-    load(DictPath)
-else
-    % Si no existe hay que crearlo
-    DataDict = containers.Map();
-    save(DictPath, 'DataDict')
-end
+    Patient = PatientList{iPatient};
+    dataFolder = fullfile(dataFolder_all, Patient, sportType, angleType, location);
 
-%% Bucle para leer los archivos uno por uno
-%% COORDENADA X
+    dataFiles = dir(fullfile(dataFolder, '*.csv'));
 
-for iDataFile = 1:numel(dataFiles)
-    file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo 
-    % Coger nombre y crear nombre del .mat
-    [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
+    % Crear una variable de celdas para almacenar los datos de los archivos CSV
+    data_cell = cell(length(dataFiles), 1);
+    DictPath = fullfile(dataFolder,strcat('DataDict_', sportType, '_', angleType, '.mat'));
 
-    if isKey(DataDict, cleanName)
-        % mirar si esta ya en diccionario
-        gyroData = DataDict(cleanName);
+    %% CARGAR .MAT
+    % Mirar si existe diccionario
+    if exist(DictPath)
+        load(DictPath)
     else
-% Si no esta guardado, cargar el csv y añadir al diccionario y guardarlo
-    gyroData = gyroData_csv2struct(file_path); 
-        % Normalizar muestras entre ... y ...
-    gyroData_norm = NormalizarFunction(gyroData, 0, 100);
-        
-        % añadir al diccionario muestras ya normalizadas
-    DataDict(cleanName) = gyroData_norm;
-    save(DictPath, 'DataDict'); 
+        % Si no existe hay que crearlo
+        DataDict = containers.Map();
+        save(DictPath, 'DataDict')
     end
 
-    t = DataDict(cleanName).time;
-    eje_vert = DataDict(cleanName).x;
-    label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
-    plot(t, eje_vert, 'DisplayName', label)
-    hold on
-    title('Euler Data Pitch Squats Lateral (External) Collateral Ligament Right Knee')
-    xlabel('Time (s)')
-    ylabel('Angle(°)')
-end
-grid on
+    %% Bucle para leer los archivos uno por uno
+    %% COORDENADA X
+    figure
+    for iDataFile = 1:numel(dataFiles)
+        file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo
+        % Coger nombre y crear nombre del .mat
+        [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
 
-% Calcular promedios con muestras normalizadas
-% COORDENADA X
-% PROMEDIO COORDENADA X
-hold on
-gyroData_all = load(DictPath);
-gyroData_all = gyroData_all.DataDict;
-[promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
-plot(promedio.time,promedio.x,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
+        if isKey(DataDict, cleanName)
+            % mirar si esta ya en diccionario
+            gyroData = DataDict(cleanName);
+        else
+            % Si no esta guardado, cargar el csv y añadir al diccionario y guardarlo
+            gyroData = gyroData_csv2struct(file_path);
+            % Normalizar muestras entre ... y ...
+            gyroData_norm = NormalizarFunction(gyroData, 0, 100);
 
-legend('show','AutoUpdate','off'); 
+            % añadir al diccionario muestras ya normalizadas
+            DataDict(cleanName) = gyroData_norm;
+            save(DictPath, 'DataDict');
+        end
 
-%% COORDENADA Y
-figure
-for iDataFile = 1:numel(dataFiles)
-    file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo
-    % Almacenar los datos en la celda correspondiente
-    % Hacer lo que necesites con los datos...
-    
-    % Coger nombre y crear nombre del .mat
-    [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
-%     filePathMat = fullfile(dataFolder, strcat(cleanName, '.mat'));
-    
-    if isKey(DataDict, cleanName)
-        % mirar si esta ya en diccionario
-        gyroData = DataDict(cleanName);
-    else
-        % Si no esta guardado, cargar el csv y añadir al diccionario y
-        % guardarlo
-        gyroData = gyroData_csv2struct(file_path); 
-        
-        % Normalizar muestras entre ... y ...
-        gyroData_norm = NormalizarFunction(gyroData, 0, 100);
-        
-        % añadir al diccionario muestras ya normalizadas
-        DataDict(cleanName) = gyroData_norm;
-        save(DictPath, 'DataDict');
+        t = DataDict(cleanName).time;
+        eje_vert = DataDict(cleanName).x;
+        label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
+        plot(t, eje_vert, 'DisplayName', label)
+        hold on
+        title('Gyro x delante ')
+        xlabel('Time (s)')
+        ylabel('Velocidad angular')
     end
-    t = DataDict(cleanName).time;
-    eje_vert = DataDict(cleanName).y;
-    label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
-    plot(t, eje_vert, 'DisplayName', label)
+    grid on
+
+    % Calcular promedios con muestras normalizadas
+    % COORDENADA X
+    % PROMEDIO COORDENADA X
     hold on
-    title('Euler Data Roll Squats Lateral (External) Collateral Ligament Right Knee')
-    xlabel('Time (s)')
-    ylabel('Angle(°)')
-end
+    gyroData_all = load(DictPath);
+    gyroData_all = gyroData_all.DataDict;
+    [promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
+    plot(promedio.time,promedio.x,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
 
-% PROMEDIO COORDENADA Y
-grid on
-hold on
-gyroData_all = load(DictPath);
-gyroData_all = gyroData_all.DataDict;
-[promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
-plot(promedio.time,promedio.y,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
+    legend('show','AutoUpdate','off');
 
-legend('show','AutoUpdate','off'); 
-%% COORDENADA Z
-figure
-for iDataFile = 1:numel(dataFiles)
-    file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo
-    % Almacenar los datos en la celda correspondiente
-    % Hacer lo que necesites con los datos...
-    
-    % Coger nombre y crear nombre del .mat
-    [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
-%     filePathMat = fullfile(dataFolder, strcat(cleanName, '.mat'));
-    
-    if isKey(DataDict, cleanName)
-        % mirar si esta ya en diccionario
-        gyroData = DataDict(cleanName);
-    else
-        % Si no esta guardado, cargar el csv y añadir al diccionario y
-        % guardarlo
-        gyroData = gyroData_csv2struct(file_path); 
-        
-        % Normalizar muestras entre ... y ...
-        gyroData_norm = NormalizarFunction(gyroData, 0, 100);
-        
-        % añadir al diccionario muestras ya normalizadas
-%         DataDict(cleanName) = gyroData;
+    %% COORDENADA Y
+    figure
+    for iDataFile = 1:numel(dataFiles)
+        file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo
+        % Almacenar los datos en la celda correspondiente
+        % Hacer lo que necesites con los datos...
 
-        DataDict(cleanName) = gyroData_norm;
-        save(DictPath, 'DataDict');
+        % Coger nombre y crear nombre del .mat
+        [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
+        %     filePathMat = fullfile(dataFolder, strcat(cleanName, '.mat'));
+
+        if isKey(DataDict, cleanName)
+            % mirar si esta ya en diccionario
+            gyroData = DataDict(cleanName);
+        else
+            % Si no esta guardado, cargar el csv y añadir al diccionario y
+            % guardarlo
+            gyroData = gyroData_csv2struct(file_path);
+
+            % Normalizar muestras entre ... y ...
+            gyroData_norm = NormalizarFunction(gyroData, 0, 100);
+
+            % añadir al diccionario muestras ya normalizadas
+            DataDict(cleanName) = gyroData_norm;
+            save(DictPath, 'DataDict');
+        end
+        t = DataDict(cleanName).time;
+        eje_vert = DataDict(cleanName).y;
+        label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
+        plot(t, eje_vert, 'DisplayName', label)
+        hold on
+        title('gyro y delante')
+        xlabel('Time (s)')
+        ylabel('Velocidad angular')
     end
-    t = DataDict(cleanName).time;
-    eje_vert = DataDict(cleanName).z;
-    label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
-    plot(t, eje_vert, 'DisplayName', label)
+
+    % PROMEDIO COORDENADA Y
+    grid on
     hold on
-    title('Euler Data Yaw Squats Lateral (External) Collateral Ligament Right Knee')
-    xlabel('Time (s)')
-    ylabel('Angle(°)')
+    gyroData_all = load(DictPath);
+    gyroData_all = gyroData_all.DataDict;
+    [promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
+    plot(promedio.time,promedio.y,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
+
+    legend('show','AutoUpdate','off');
+    %% COORDENADA Z
+    figure
+    for iDataFile = 1:numel(dataFiles)
+        file_path = fullfile(dataFolder, dataFiles(iDataFile).name); % ruta completa del archivo
+        % Almacenar los datos en la celda correspondiente
+        % Hacer lo que necesites con los datos...
+
+        % Coger nombre y crear nombre del .mat
+        [~,cleanName] = fileparts(strrep(dataFiles(iDataFile).name, ' ', '_'));
+        %     filePathMat = fullfile(dataFolder, strcat(cleanName, '.mat'));
+
+        if isKey(DataDict, cleanName)
+            % mirar si esta ya en diccionario
+            gyroData = DataDict(cleanName);
+        else
+            % Si no esta guardado, cargar el csv y añadir al diccionario y
+            % guardarlo
+            gyroData = gyroData_csv2struct(file_path);
+
+            % Normalizar muestras entre ... y ...
+            gyroData_norm = NormalizarFunction(gyroData, 0, 100);
+
+            % añadir al diccionario muestras ya normalizadas
+            DataDict(cleanName) = gyroData_norm;
+            save(DictPath, 'DataDict');
+        end
+        t = DataDict(cleanName).time;
+        eje_vert = DataDict(cleanName).z;
+        label = strcat('Muestra', num2str(iDataFile)); % crea una variable para indicar el numero de muestra
+        plot(t, eje_vert, 'DisplayName', label)
+        hold on
+        title('gyro z delante')
+        xlabel('Time (s)')
+        ylabel('Velocidad angular')
+    end
+
+    % PROMEDIO COORDENADA Z
+    grid on
+    hold on
+    gyroData_all = load(DictPath);
+    gyroData_all = gyroData_all.DataDict;
+    [promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
+    plot(promedio.time,promedio.z,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
+
+    legend('show','AutoUpdate','off');
+
 end
-
-% PROMEDIO COORDENADA Z
-grid on
-hold on
-gyroData_all = load(DictPath);
-gyroData_all = gyroData_all.DataDict;
-[promedio, gyroData_all_interp] = promedioFunction(gyroData_all);
-plot(promedio.time,promedio.z,'k-','LineWidth',1.75, 'DisplayName', 'Promedio')
-
-legend('show','AutoUpdate','off'); 
-
